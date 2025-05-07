6af0573cb68edb3f3c03b8b0ad558b280c7ab00f
Arkajit Paul <Arka@tanmayees-MacBook-Air-2.local>
2025-05-07 12:29:23 -0400

    Adding stats

---
+++ stats.py
@@ -0,0 +1,137 @@
+'''
+Project Name:  "Barca Manager: Build Your Legacy"
+Student Name: Arkajit Paul
+Date: Wednesday, 07 May 2025
+Course: ICS3U-02
+This is a program exploring the world of football management, mainly in the context of FC Barcelona. FC Barcelona is one of the best clubs in the history of soccer and 
+has a rich history of success and talent. The club has won a total of 27 La Liga titles, 31 Copa del Rey titles, 13 Supercopa de España titles, and 5 UEFA Champions League titles. Throughout this progarm
+it will exlore managing Barcelona and making descions that will impact the club's future.. 
+'''
+import pygame
+import random
+import time
+import sys
+import os
+import math
+
+# Dictionaries with player names
+
+Goalkeepers=["Marc-Andre ter Stegan", "Inaki Pena", "Wojciech Szczesny", "Ander Astralaga", "Deigo Kochen", "Aron Yaakobishvili"]
+Centerbacks=["Pau Cubarsi", "Ronald Araujo", "Inigo Martinez", "Andreas Christensen", "Sergi Dominguez", "Eric Garcia"]
+Rightbacks=["Jules Kounde", "Hector Fort"]
+Leftbacks=["Alejandro Balde", "Gerad Martin"]
+Midfielders=["Gavi", "Pedri", "Pablo Torre", "Fermin Lopez", "Marc Casado", "Dani Olmo", "Frenkie De Jong", "Marc Barnal"]
+Wingers=["Ansu Fati", "Raphinha", "Lamine Yamal", "Pau Victor"]
+Strikers=["Robert Lewandowski", "Ferran Torres"]
+
+#Dictionary with player ratings based on how much Flick starts them
+keeper_ratings = {"Marc-Andre ter Stegan": 85, "Inaki Pena": 72, "Wojciech Szczesny": 89, "Ander Astralaga": 60, "Deigo Kochen": 60, "Aron Yaakobishvili": 50}
+centerback_ratings = {"Pau Cubarsi": 95, "Ronald Araujo": 90, "Inigo Martinez": 97, "Andreas Christensen": 85, "Sergi Dominguez": 73, "Eric Garcia": 87}
+rightback_ratings = {"Jules Kounde": 95, "Hector Fort": 80}
+leftback_ratings = {"Alejandro Balde": 95, "Gerad Martin": 88}
+midfielder_ratings = {"Gavi": 88, "Pedri": 99, "Pablo Torre": 80, "Fermin Lopez": 85, "Marc Casado": 92, "Dani Olmo": 92, "Frenkie De Jong": 95, "Marc Barnal": 80}
+winger_ratings = {"Ansu Fati": 80, "Raphinha": 99, "Lamine Yamal": 99, "Pau Victor": 82}
+striker_ratings = {"Robert Lewandowski": 95, "Ferran Torres": 90}
+
+#Dictionaries on stats of players
+goalkeeper_stats = {
+    "Marc-Andre ter Stegan": {
+        "diving": 92,
+        "handling": 65,
+        "kicking": 90,
+        "reflexes": 90,
+        "speed": 82,
+        "positioning": 88
+    },
+    "Inaki Pena": {
+        "diving": 78,
+        "handling": 75,
+        "kicking": 72,
+        "reflexes": 80,
+        "speed": 70,
+        "positioning": 76
+    },
+    "Wojciech Szczesny": {
+        "diving": 86,
+        "handling": 84,
+        "kicking": 80,
+        "reflexes": 88,
+        "speed": 72,
+        "positioning": 85
+    },
+    "Ander Astralaga": {
+        "diving": 65,
+        "handling": 62,
+        "kicking": 60,
+        "reflexes": 68,
+        "speed": 65,
+        "positioning": 63
+    },
+    "Deigo Kochen": {
+        "diving": 65,
+        "handling": 63,
+        "kicking": 61,
+        "reflexes": 67,
+        "speed": 66,
+        "positioning": 64
+    },
+    "Aron Yaakobishvili": {
+        "diving": 55,
+        "handling": 52,
+        "kicking": 50,
+        "reflexes": 58,
+        "speed": 54,
+        "positioning": 53
+    }
+}
+
+centerback_stats = {
+    "Pau Cubarsi": {
+        "pace": 90,
+        "shooting": 75,
+        "passing": 95,
+        "dribbling": 92,
+        "defending": 88,
+        "physicality": 88
+    },
+    "Ronald Araujo": {
+        "pace": 97,
+        "shooting": 77,
+        "passing": 88,
+        "dribbling": 80,
+        "defending": 92,
+        "physicality": 92
+    },
+    "Inigo Martinez": {
+        "pace": 85,
+        "shooting": 80,
+        "passing": 97,
+        "dribbling": 85,
+        "defending": 97,
+        "physicality": 92
+    },
+    "Andreas Christensen": {
+        "pace": 85,
+        "shooting": 74,
+        "passing": 85,
+        "dribbling": 50,
+        "defending": 70,
+        "physicality": 80
+    },
+    "Sergi Dominguez": {
+        "pace": 85,
+        "shooting": 60,
+        "passing": 70,
+        "dribbling": 50,
+        "defending": 70,
+        "physicality": 80
+    },
+    "Eric Garcia": {
+        "pace": 85,
+        "shooting": 60,
+        "passing": 70,
+        "dribbling": 50,
+        "defending": 70,
+        "physicality": 80
+    }
+}
